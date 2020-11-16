717a4f423a29809396616f72470c1c373dda38a0
Jason Timm <jaetimm@gmail.com>
2020-11-16 08:20:08 -0700

    re

--- R/get_legis_geos.R
+++ R/get_legis_geos.R
@@ -2,26 +2,26 @@
 
 
 get_legis_geos <- function (state) {
-  
-  states <- tigris::states(cb = T) 
+
+  states <- tigris::states(cb = T)
   state1 <- subset(states, NAME == state)
-  
+
   upper_house <- tigris::state_legislative_districts(
-    state = state, house = 'upper', cb = T) %>%
-    mutate(district_code = as.integer(substr(SLDUST, 2, 3))) 
-  
+    state = state, house = 'upper', cb = T)
+  upper_house$district_code <- as.integer(substr(upper_house$SLDUST, 2, 3))
+
   lower_house<- tigris::state_legislative_districts(
-    state = state, house = 'lower', cb = T) %>%
-    mutate(district_code = as.integer(substr(SLDLST, 2, 3))) 
-  
+    state = state, house = 'lower', cb = T)
+  lower_house$district_code <- as.integer(substr(lower_house$SLDUST, 2, 3))
+
   counties <- tigris::counties(state = state, cb = T)
-  cds_all <- tigris::congressional_districts(cb = T) 
-  cds <- subset(cds_all, STATEFP == state1$GEOID) 
+  cds_all <- tigris::congressional_districts(cb = T)
+  cds <- subset(cds_all, STATEFP == state1$GEOID)
   places <- tigris::places(state = state, cb =T)
-  
-  list('upper_house' = upper_house, 
-       'lower_house' = lower_house, 
-       'counties' = counties, 
-       'cds' = cds, 
+
+  list('upper_house' = upper_house,
+       'lower_house' = lower_house,
+       'counties' = counties,
+       'cds' = cds,
        'places' = places)
 }
